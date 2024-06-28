60d791fb2fbfad193ce2a3a225fc2aa38df1f301
joaogabriellyra <jgabriellyra@hotmail.com>
2024-06-28 00:20:03 -0300

    the sidebar is done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [{
+      hostname: 'plus.unsplash.com'
+    },
+    {
+      hostname: 'github.com'
+    }
+  ],
+  },
+
+};
 
 export default nextConfig;
---
+++ src/app/components/sidebar.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+import styles from './sidebar.module.css';
+import Link from 'next/link';
+
+export default function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <Image
+        className={styles.cover} 
+        src='https://plus.unsplash.com/premium_photo-1685086785636-2a1a0e5b591f?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        alt='random image'
+        width={500}
+        height={100}
+      />
+
+      <div className={styles.profile}>
+        <Image 
+          className={styles.avatar}
+          src='https://github.com/joaogabriellyra.png'
+          alt='avatar image'
+          width={48}
+          height={48}
+        />
+        <strong>João Gabriel Lyra</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+
+        <Link href='#'>
+          Editar seu perfil
+        </Link>
+      </footer>
+    </aside>
+  )
+}
